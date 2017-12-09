cb06e410adb640d5f68fa5c76331136b44821580
pc-magas <pc-magas@users.noreply.github.com>
2017-12-09 16:10:49 +0200

    Progress so far

--- App/App.cpp
+++ App/App.cpp
@@ -14,14 +14,14 @@ int main(int argc, char const *argv[]) {
 
     int result=0,a=1,b=2;
 
-    std::cout << "Calling secure ad" << std::endl
+    std::cout << "Calling secure ad" << std::endl;
     sgx_status_t status = sum(global_eid, &a,&b,&result);
 
     std::cout << "Enclave Returned status: " << status << std::endl;
 
     if (status != SGX_SUCCESS) {
         std::cerr << "Enclave have not called sucessfully" << std::endl;
-        return 1
+        return 1;
     }
 
     std::cout << "Sum result: " << result << std::endl;
