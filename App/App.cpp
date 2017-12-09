aa9f0cee435f7c6e9583fff2e17116acb367045e
pc-magas <pc-magas@users.noreply.github.com>
2017-12-09 16:05:39 +0200

    Progress so far

---
+++ App/App.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Enclave_u.h"
+#include "sgx_urts.h"
+#include "sgx_utils/sgx_utils.h"
+
+/* Global EID shared by multiple threads */
+sgx_enclave_id_t global_eid = 0;
+
+int main(int argc, char const *argv[]) {
+    if (initialize_enclave(&global_eid, "enclave.token", "enclave.signed.so") < 0) {
+        std::cerr << "Fail to initialize enclave." << std::endl;
+        return 1;
+    }
+
+    int result=0,a=1,b=2;
+
+    std::cout << "Calling secure ad" << std::endl
+    sgx_status_t status = sum(global_eid, &a,&b,&result);
+
+    std::cout << "Enclave Returned status: " << status << std::endl;
+
+    if (status != SGX_SUCCESS) {
+        std::cerr << "Enclave have not called sucessfully" << std::endl;
+        return 1
+    }
+
+    std::cout << "Sum result: " << result << std::endl;
+    return 0;
+}
---
+++ Enclave/Enclave.cpp
@@ -0,0 +1,6 @@
+#include "Enclave_t.h"
+
+void sum(int *a, int *b, int *sum)
+{
+  *sum = *a + *b;
+}
